1c6d80238a98ad2aaf297e96ec72678d20e3437e
luo-songtao <ryomawithlst@gmail.com>
2020-01-15 20:03:10 +0800

    dynamic_programming: cut_rod.py

--- dynamic_programming/cut_rod.py
+++ dynamic_programming/cut_rod.py
@@ -21,9 +21,47 @@ def naive_recu_cut_rod(price, n):
         value = max(value, price[i] + naive_recu_cut_rod(price, n-i))
     return value
 
-def memoized_cut_rod(price, n):
-    
 
+def memoized_cut_rod_aux(price, n, memo):
+    if memo[n] > 0:
+        return memo[0]
+    
+    if n == 0:
+        q = 0
+    else: 
+        q = - math.inf
+        for i in range(1, n+1):
+            q = max(q, price[i] + memoized_cut_rod_aux(price, n-i, memo))
+    memo[n] = q
+    return q
+
+
+def memoized_cut_rod(price, n):
+    """
+    >>> price = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    >>> memoized_cut_rod(price, len(price)-1)
+    30
+    """
+    memo = [-math.inf for i in range(n+1)]
+    return memoized_cut_rod_aux(price, n, memo)
+
+
+def buttom_up_cut_rod(price, n):
+    """
+    >>> price = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    >>> buttom_up_cut_rod(price, len(price)-1)
+    30
+    """
+
+    memo = [-math.inf for i in range(n+1)]
+    memo[0] = 0
+
+    for i in range(1, n+1):
+        q = -math.inf
+        for j in range(1, i+1):
+            q = max(q, price[j]+memo[i-j])
+        memo[i] = q
+    return memo[n]
 
 if __name__ == "__main__":
     import doctest
