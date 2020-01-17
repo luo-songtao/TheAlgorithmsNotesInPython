39fd86247f47b1b90cbabacaa517b63819ceabfb
luo-songtao <ryomawithlst@gmail.com>
2020-01-17 19:01:35 +0800

    update

---
+++ data-structures/heaps/mergeable_min_heap.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+Author: luo-songtao
+可合并最小堆
+"""
+
+
+class MergeableMinHeap:
+    
+    def create(self):
+        pass
+    
+    def insert(self, x):
+        pass
+    
+    def minmum(self):
+        pass
+    
+    def extract_min(self):
+        pass
+    
+    def union(self, heap: MergeableMinHeap):
+        pass
